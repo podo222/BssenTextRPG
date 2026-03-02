d76ecf1e6382075b4e7f3ff7d104e01420e634cb
Podo <podo99150@gmail.com>
2026-03-02 18:30:02 +0900

    feat(player): Character 기본 추상 클래스 생성

---
+++ Models/Character.cs
@@ -0,0 +1,42 @@
+namespace BssenTextRPG.Models;
+
+//캐릭터 기본 추상 클래스
+public abstract class Character
+{
+    #region 프로퍼티
+
+    //protected : 해당 클래스 내에서는 접근 가능하지만, 외부에서는 접근 불가능한 접근 제한자, 자식 클래스에서는 접근 가능
+    public string Name { get; protected set; }
+    public int CurrentHp { get; protected set; }
+    public int MaxHp { get; protected set; }
+    public int CurrentMp { get; protected set; }
+    public int MaxMp { get; protected set; }
+    public int AttackPower { get; protected set; }
+    public int Defense { get; protected set; }
+    public int Level { get; protected set; }
+    //생존 여부
+    public bool IsAlive => CurrentHp > 0; //현재 HP가 0보다 크면 생존, 그렇지 않으면 사망
+
+    #endregion
+
+    #region 생성자
+
+    protected Character(string name, int maxHp, int attackPower, int defense, int level)
+    {
+        Name = name;
+        MaxHp = maxHp;
+        CurrentHp = maxHp; //초기 HP는 최대 HP로 설정
+        CurrentMp = MaxMp;
+        AttackPower = attackPower;
+        Defense = defense;
+        Level = level;
+    }
+
+    #endregion
+
+    #region 메서드
+
+    
+
+    #endregion
+}
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ class Program
     {
         //콘솔 인코딩 설정 (한글 지원)
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        
+         
         //게임 저장 여부 확인
         //게임 로드 및 새 게임 시작
         
