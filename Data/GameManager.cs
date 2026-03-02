3d79afcdae34c29060409a8a8f09ca6b0ca72c6c
Podo <podo99150@gmail.com>
2026-03-02 19:51:01 +0900

    feat(player): 직업별 초기 스텟 및 테스트 코드 추가

--- Data/GameManager.cs
+++ Data/GameManager.cs
@@ -108,6 +108,12 @@ public class GameManager
         //입력한 이름과 선택한 직업으로 플레이어 캐릭터 생성
         Player = new Player(name, job);
         Console.WriteLine($"\n{name}님, {job}직업으로 캐릭터가 생성되었습니다!");
+        
+        //Test Code
+        Console.WriteLine($"Player HP: {Player.CurrentHp}");
+        Console.WriteLine($"Player MP: {Player.CurrentMp}");
+        Console.WriteLine($"Player ATX: {Player.AttackPower}");
+        Console.WriteLine($"Player DEF: {Player.Defense}");
     }
     #endregion
 }
--- Models/Player.cs
+++ Models/Player.cs
@@ -15,10 +15,10 @@ public class Player : Character
     
     public Player(string name, JobType job) : base(
         name:name,
-        maxHp:100,
-        maxMp:50,
-        attackPower:20, 
-        defense:10, 
+        maxHp:GetInitHp(job),
+        maxMp:GetInitMp(job),
+        attackPower:GetInitAttack(job), 
+        defense:GetInitDefense(job), 
         level:1)
     {
         Job = job;
@@ -26,5 +26,53 @@ public class Player : Character
     }
     
     #endregion
+
+    #region  직업별 초기 스텟
+
     
+    private static int GetInitHp(JobType job)
+    {
+        switch (job)
+        {
+            case JobType.Warrior: return 150;
+            case JobType.Archer: return 100;
+            case JobType.Wizard: return 80;
+            default: return 100;
+        }
+    }
+
+    private static int GetInitMp(JobType job)
+    {
+        switch (job)
+        {
+            case JobType.Warrior: return 30;
+            case JobType.Archer: return 50;
+            case JobType.Wizard: return 100;
+            default: return 30;
+        }
+    }
+
+    // C# 8.0 이상에서 사용 가능한 switch 식 표현식
+    // expression-body 문법 사용하여 간결하게 표현
+    private static int GetInitAttack(JobType job) =>
+        job switch
+        {
+            //case 생략 가능
+            JobType.Warrior => 20,
+            JobType.Archer => 30,
+            JobType.Wizard => 40,
+            _ => 20 //default 역할
+        };
+    
+    private static int GetInitDefense(JobType job) =>
+        job switch
+        {
+            JobType.Warrior => 15,
+            JobType.Archer => 10,
+            JobType.Wizard => 5,
+            _ => 15
+        };
+
+    #endregion
+
 }
