9288a9c4650c7f694a017b3dfddaaffd03f4503e
Podo <podo99150@gmail.com>
2026-03-04 10:58:41 +0900

    feat(battle): 도망 시도 성공 확률 추가 및 전투 시스템 초기화

--- Systems/BattleSystem.cs
+++ Systems/BattleSystem.cs
@@ -104,8 +104,18 @@ public class BattleSystem
                     return true;
                 
                 case "3":
-                    //도망 시도
-                    return false;
+                    //도망 시도 : (성공 확률 50%)
+                    Random random = new Random();
+                    if (random.NextDouble() < 0.5)
+                    {
+                        Console.WriteLine("도망에 성공했습니다!");
+                        return false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n도망에 실패했습니다.");
+                        return true;
+                    }
                 
                 default:
                     Console.WriteLine("잘못된 입력입니다. 다시 선택해주세요.");
